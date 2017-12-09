c20bb4f045bda5ae7527328289133b6c4b5c3cc7
erivera23 <riveraefrain5@gmail.com>
2017-12-08 22:33:08 -0600

    Menú y navegación para móviles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click( function (ev){
+    const panel = $(this).data('toggleDia')
+    $('#horarios-tabs').foundation('selectTab', panel);
+})
+
+const $offCanvas = $('#offCanvas')
+$offCanvas.find('li').click( function (ev){
+    $offCanvas.foundation('close');
+})
