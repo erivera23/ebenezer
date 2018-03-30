85c8a53255da50a8c03b00d60212e435ccc3c8cc
erivera23 <riveraefrain5@gmail.com>
2018-03-30 17:24:51 -0600

    Formulario contacto envía correo

---
+++ js/config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDakEigYOmSUqcKM0I9i7xgVHNXMORmCko",
+    authDomain: "letrasong-a9387.firebaseapp.com",
+    databaseURL: "https://letrasong-a9387.firebaseio.com",
+    projectId: "letrasong-a9387",
+    storageBucket: "letrasong-a9387.appspot.com",
+    messagingSenderId: "125673707137"
+  };
+  firebase.initializeApp(config);
